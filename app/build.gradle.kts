bc6f619f4f374bb731d45183fc5c6cad84238dca
lavialireza <alirezalavi65@gmail.com>
2026-08-12 16:36:57 +0330

    Update build.gradle.kts
    افزودن Gson برای تبدیل JSON

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,6 +39,7 @@ android {
 }
 
 dependencies {
+    implementation 'com.google.code.gson:gson:2.10.1'
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.1")
     implementation("androidx.activity:activity-compose:1.9.0")
