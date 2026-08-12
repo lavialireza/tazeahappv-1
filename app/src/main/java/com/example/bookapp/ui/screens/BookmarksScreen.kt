ee4febfd5df3da3c1053fcf12a602920cdf4c6b2
lavialireza <alirezalavi65@gmail.com>
2026-08-12 18:43:18 +0330

    Update BookmarksScreen.kt

--- app/src/main/java/com/example/bookapp/ui/screens/BookmarksScreen.kt
+++ app/src/main/java/com/example/bookapp/ui/screens/BookmarksScreen.kt
@@ -1,5 +1,5 @@
 package com.example.bookapp.ui.screens
-
+import androidx.compose.material3.Divider
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
