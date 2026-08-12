777a7d6ed0aad9491774f82a4ccd5e46339383e4
lavialireza <alirezalavi65@gmail.com>
2026-08-13 00:17:17 +0330

    Update InfoScreens.kt

--- app/src/main/java/com/example/bookapp/ui/screens/InfoScreens.kt
+++ app/src/main/java/com/example/bookapp/ui/screens/InfoScreens.kt
@@ -1,5 +1,6 @@
 package com.example.bookapp.ui.screens
-
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
