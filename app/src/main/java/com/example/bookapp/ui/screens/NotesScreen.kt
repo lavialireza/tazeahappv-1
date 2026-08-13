1993cfc815247f968f46b16455629e47c05073b4
lavialireza <alirezalavi65@gmail.com>
2026-08-13 13:40:10 +0330

    Update NotesScreen.kt

--- app/src/main/java/com/example/bookapp/ui/screens/NotesScreen.kt
+++ app/src/main/java/com/example/bookapp/ui/screens/NotesScreen.kt
@@ -5,7 +5,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.*
@@ -31,7 +31,7 @@ fun NotesScreen(
                 title = { Text("دفتر یادداشت") },
                 navigationIcon = {
                     IconButton(onClick = onBack) {
-                        Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "بازگشت")
+                        Icon(Icons.Default.ArrowBack, contentDescription = "بازگشت")
                     }
                 }
             )
