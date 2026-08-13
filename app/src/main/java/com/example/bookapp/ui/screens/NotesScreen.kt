726422edfcd830b6797bc967dd060e1c5ebc2da8
lavialireza <alirezalavi65@gmail.com>
2026-08-13 11:18:16 +0330

    Update NotesScreen.kt

--- app/src/main/java/com/example/bookapp/ui/screens/NotesScreen.kt
+++ app/src/main/java/com/example/bookapp/ui/screens/NotesScreen.kt
@@ -1,6 +1,5 @@
 package com.example.bookapp.ui.screens
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
+
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
