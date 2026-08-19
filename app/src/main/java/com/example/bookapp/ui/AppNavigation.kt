a56891aafc0b260ebafea0750f0ccced531cc097
lavialireza <alirezalavi65@gmail.com>
2026-08-19 22:06:50 +0330

    Add files via upload

--- app/src/main/java/com/example/bookapp/ui/AppNavigation.kt
+++ app/src/main/java/com/example/bookapp/ui/AppNavigation.kt
@@ -15,6 +15,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
